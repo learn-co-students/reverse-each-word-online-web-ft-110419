a76c89c78531da0ff43015d2a470f37c42421adc
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-07 19:43:25 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|sentence| sentence.reverse!}
+  return reversed_array.join(" ")
+end
+
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|sentence| sentence.reverse!}
+  new_array.join(" ")
+end
