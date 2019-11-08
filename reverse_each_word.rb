42cb6edd80efd3764440b2400ff86a98a8b9e864
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-08 05:03:09 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,5 @@
 require 'pry'
 
 def reverse_each_word(string)
-  array = []
-  array << string.split
-  array.each { |word|
-  word.reverse
-  }
-  array.join
-binding.pry
-
+  string.split(" ").collect { |word| word.reverse}.join(" ")
 end
